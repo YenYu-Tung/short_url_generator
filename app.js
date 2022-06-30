ed42b968cc097e98f8086c8cf625c0329163217e
YenYu-Tung <74877687+YenYu-Tung@users.noreply.github.com>
2022-06-30 10:22:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+require('./config/mongoose')
+
+
 
 const app = express()
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
@@ -11,15 +12,6 @@ const routes = require('./routes')
 app.use(routes)
 
 app.use(express.static('public'))
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connected')
-})
-
 
 app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
